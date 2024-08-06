b055d4496c07c0e9e935572339aea518b787f462
Julian Cortes <juliancortes729@gmail.com>
2024-08-06 11:40:45 -0300

    Update settings.json SampleScene.unity PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,28 +8,41 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] private float forceJump = 0.5f;
+    [SerializeField] private float speed = 10.0f;
 
     private Rigidbody playerRb;
     private bool suelo;
+    public Joystick joystick;
+
+    private Animator animator;
 
     void Start()
     {
         suelo = true;       
         playerRb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Jump();
+        if(joystick.Horizontal<0 || joystick.Horizontal>0){
+            Run();
+            animator.SetBool("isRun",true);
+        }else{
+            animator.SetBool("isRun",false);
+        }
     }
 
-
     public void Jump(){
-        if(suelo && Touchscreen.current.primaryTouch.press.isPressed){
+        if(suelo){
             playerRb.AddForce(forceJump*Vector3.up,ForceMode.Impulse);
             suelo = false;
         }
+    }
+
+    public void Run(){
+        float movH = joystick.Horizontal*speed;
+        playerRb.AddForce(movH,0,0);
     }
 
     void OnCollisionEnter() => suelo = true;
